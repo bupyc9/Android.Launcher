f2cbfc70bf86acc2f0d18299a867632c0a67f154
bupyc <afanasev@worksolutions.ru>
2018-02-28 23:37:21 +0300

    init

---
+++ app/src/main/java/bupyc9/launcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package bupyc9.launcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
